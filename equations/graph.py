ce2ebe635667e5d91efd86e5650fc459558adb6e
Jack Long <ajacklong@outlook.com>
2023-11-20 14:38:43 -0500

    Fully fixed colorbar, colormap.

--- equations/graph.py
+++ equations/graph.py
@@ -1,6 +1,6 @@
 import numpy as __np
 import matplotlib.pyplot as __plt
-from matplotlib.colors import Normalize
+from matplotlib.colors import Normalize, Colormap
 from matplotlib import cm
 from equations.parameters import *
 import time
@@ -12,14 +12,13 @@ R, theta = __np.meshgrid(r, tha1)
 X = R * __np.cos(theta)
 Y = R * __np.sin(theta)
 
-cmap = __plt.get_cmap('jet',10000)
+cmap = __plt.get_cmap('jet')
 
-fig, ax = __plt.subplots()
-
-def graph_eq(sig, name = str(time.time())[7:-4], factor = 10e6, dir = "./pictures/", colorbar_ticks = "0.5 or 1, 0.2 for T"):
+# Should be 10e6? Need to figure out why/if 10x difference.
+def graph_eq(sig, name = str(time.time())[7:-4], factor = 10e5, dir = "./pictures/", colorbar_ticks = "0.5 or 1, 0.2 for T"):
     fig, ax = __plt.subplots()
     values = __np.abs(sig)
-    ax.contourf(X, Y, values)
+    ax.contourf(X, Y, values, 256)
 
     vmax = __np.ceil(__np.amax(values)/factor)
     vmin = __np.floor(__np.amin(values)/factor)
@@ -27,7 +26,7 @@ def graph_eq(sig, name = str(time.time())[7:-4], factor = 10e6, dir = "./picture
     norm = Normalize(vmin=vmin, vmax=vmax)
     sm = cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
-    fig.colorbar(sm, ticks=__np.arange(vmin,vmax,.25), boundaries=__np.arange(vmin,vmax,.001), ax=ax)
+    fig.colorbar(sm, ticks=__np.arange(0,vmax,.25), boundaries=__np.arange(0,vmax,.001), ax=ax)
 
     __plt.gca().set_aspect("equal")
     __plt.xlabel("z (mm)")
@@ -35,14 +34,16 @@ def graph_eq(sig, name = str(time.time())[7:-4], factor = 10e6, dir = "./picture
     fig.savefig(dir + name + ".png")
 
 def graph_temp_gr(values, shift = 21, name = str(time.time())[7:-4], dir = "./pictures/"):
-    ax.contourf(X, Y, values - shift)
-    vmax = __np.floor(__np.amax(__np.abs(values)))
-    vmin = __np.floor(__np.amin(__np.abs(values)))
+    fig, ax = __plt.subplots()
+    ax.contourf(X, Y, values - shift, 256)
+
+    vmax = __np.ceil(__np.amax(__np.abs(values))) - 21
+    vmin = __np.floor(__np.amin(__np.abs(values))) - 21
 
     norm = Normalize(vmin=vmin, vmax=vmax)
     sm = cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
-    #fig.colorbar(sm, ticks=__np.arange(vmin,vmax,.25), boundaries=__np.arange(vmin,vmax,.001), ax=ax)
+    fig.colorbar(sm, ticks=__np.arange(0,vmax,.25), boundaries=__np.arange(0,vmax,.001), ax=ax)
     
     __plt.gca().set_aspect("equal")
     fig.savefig(dir + name + ".png")
