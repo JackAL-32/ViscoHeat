dc70720bbec8649fa6fc3ff224ff18ef43040746
Jack Long <ajacklong@outlook.com>
2023-11-29 12:37:29 -0500

    Added tickstep control and graph title

--- equations/graph.py
+++ equations/graph.py
@@ -15,7 +15,7 @@ Y = R * __np.sin(theta)
 cmap = __plt.get_cmap('jet')
 
 # Should be 10e6? Need to figure out why/if 10x difference.
-def graph_eq(sig, name = str(time.time())[7:-4], factor = 10e5, dir = "./pictures/", colorbar_ticks = "0.5 or 1, 0.2 for T"):
+def graph_eq(sig, name = str(time.time())[7:-4], factor = 10e5, dir = "./pictures/"):
     fig, ax = __plt.subplots()
     values = __np.abs(sig)
     ax.contourf(X, Y, values, 256)
@@ -26,12 +26,21 @@ def graph_eq(sig, name = str(time.time())[7:-4], factor = 10e5, dir = "./picture
     norm = Normalize(vmin=vmin, vmax=vmax)
     sm = cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
-    fig.colorbar(sm, ticks=__np.arange(0,vmax,.25), boundaries=__np.arange(0,vmax,.001), ax=ax)
+
+    # colorbar_ticks = "0.5 or 1, 0.2 for T"
+    # From last year ^
+    if(name == "rR"): tickstep = 1
+    else: tickstep = .5
+
+    if (name == "q1"): title = "Heat "
+    else: title = "Stress "
+
+    fig.colorbar(sm, ticks=__np.arange(0,vmax,tickstep), boundaries=__np.arange(0,vmax,.001), ax=ax)
 
     __plt.gca().set_aspect("equal")
     __plt.xlabel("z (mm)")
     __plt.ylabel("x (mm)")
-    __plt.title(name)
+    __plt.title(title + name)
     fig.savefig(dir + name + ".png")
 
 # shift is T0?
@@ -45,7 +54,10 @@ def graph_temp_gr(values, shift = 21, name = str(time.time())[7:-4], dir = "./pi
     norm = Normalize(vmin=vmin, vmax=vmax)
     sm = cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
-    fig.colorbar(sm, ticks=__np.arange(0,vmax,.25), boundaries=__np.arange(0,vmax,.001), ax=ax)
+    
+    tickstep = .2 # From last year
+
+    fig.colorbar(sm, ticks=__np.arange(0,vmax,tickstep), boundaries=__np.arange(0,vmax,.001), ax=ax)
     
     __plt.gca().set_aspect("equal")
     __plt.title(name)
