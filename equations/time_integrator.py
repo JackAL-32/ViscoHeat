ff11e20e8c1bfd8e7497124cefe58b5e9f3785fe
JackAL-32 <ajacklong@outlook.com>
2024-03-06 14:40:07 -0500

    Moving to desktop

--- equations/time_integrator.py
+++ equations/time_integrator.py
@@ -2,46 +2,35 @@ import numpy as __np
 from equations.parameters import *
 from numpy import sin, cos
 
-def get_q1(sigrr, sigphiphi, sigthatha, sigrtha):
-    from numpy import angle, abs
-    # This is the equation for Delta R R
-    drr = (angle(E1)) + (angle(sigrr)) - (angle((sigrr) - ((nu1) * ((sigthatha) + (sigphiphi)))))
-    
-    # This is the equation for Delta theta theta
-    dthatha = (angle(E1)) + (angle(sigthatha)) - (angle((sigthatha) - ((nu1) * ((sigrr) + (sigphiphi)))))
-    
-    # This is the equation for Delta phi phi
-    dphiphi = (angle(E1)) + (angle(sigphiphi)) - (angle((sigphiphi) - ((nu1) * ((sigrr) + (sigthatha)))))
-    
-    # This is the equation for Delta R theta
-    drtha = (angle(E1)) - (angle(1 + nu1))
-    
-    # This is the equation for Xi R R
-    xirr = (__np.sin(drr)) * (abs(sigrr)) * (abs((sigrr) - ((nu1) * ((sigthatha) + (sigphiphi)))))
-    
-    # This is the equation for Xi theta theta
-    xithatha = (__np.sin(dthatha)) * (abs(sigthatha)) * (abs((sigthatha) - ((nu1) * ((sigrr) + (sigphiphi)))))
-    
-    # This is the equation for Xi phi phi
-    xiphiphi = (__np.sin(dphiphi)) * (abs(sigphiphi)) * (abs((sigphiphi) - ((nu1) * ((sigrr) + (sigthatha)))))
-    
-    # This is the equation for Xi R theta
-    xirtha = (__np.sin(drtha)) * (abs(1 + nu1)) * ((abs(sigrtha)) * (abs(sigrtha)))
-    
-    # This is the equation for q1
-    q1 = ((w)/(2 * (abs(E1)))) * (xirr + xithatha + xiphiphi + xirtha)
-
-    return q1
-
-#----------------------------------------------------------------------------------------
-# Parameter Equations for Tempurature Gradient
-#========================================================================================
-
 def get_T(r, tha, time, gamma, dt, dr, dtha):
-    T = __np.fill( shape=(r.size, tha.size, time.size), fill_value=T0 )
-    dTdt = __np.zeros( (r.size, tha.size, time.size) )
+    
+    T = __np.fill( shape=(r.size+2, tha.size+2, time.size), fill_value=T0 ) # include necessary padding area, and fill with T0.
+    dTdt = __np.zeros((r.size, tha.size, time.size)) 
+    T34 = __np.zeros((r.size+2, tha.size+2, time.size))
+    dTdt34 = __np.zeros((r.size, tha.size, time.size))
+
+    # Initialize first timestep
+    T[r.size+1, :, 1] = 21; # Tinf for rmax boundary
+    # At initial timestep all other boundary conds are T0.
 
     for t in range(time.size()):
+        # Interior cases:
+        ptsInt = __np.where()
+        # Boundary cases:
+        # tha max
+
+        # tha min
+        # r max
+        # r min
+        # r a
+        
+
+
+
+
+
+
+
         for i in range(r.size()):
             for j in range(tha.size()):
                 if i == 0: dTdt[i,j,t] = 0 #Eqn1
